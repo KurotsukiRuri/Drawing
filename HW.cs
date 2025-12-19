181da42fea8a51cfc89e6706789801d32003149a
黒月瑠璃 <86876700+KurotsukiRuri@users.noreply.github.com>
2025-12-20 00:48:49 +0800

    Update HW.cs

--- HW.cs
+++ HW.cs
@@ -9,7 +9,7 @@ using KodakkuAssist.Module.Draw;
 
 namespace HelloWorld;
 
-[ScriptType(name: "HelloWorld", territorys: [], guid: "AF4E8C43-08BA-4C98-8DEF-70F023AF234D",
+[ScriptType(name: "HelloWorld", territorys: [], guid: "343daaf1-d3f8-4aa5-8c7f-0e08571dab33",
     version: "0.0.0.1", author: "Baka-Water77", note: null)]
 
 public class HelloWorld
