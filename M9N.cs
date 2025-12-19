35e8d808477cc97f2c63eeaa436bec4b79d1b62e
黒月瑠璃 <86876700+KurotsukiRuri@users.noreply.github.com>
2025-12-19 17:47:57 +0800

    Update M9N.cs

--- M9N.cs
+++ M9N.cs
@@ -15,97 +15,107 @@ using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using KodakkuAssist.Module.Script.Type;
 
-namespace BakaWater77.M9N;
-
-[ScriptType(name: "M9N", territorys: new[] { 1320 }, guid: "9af9ac60-1d6e-4247-a144-c6273417fea9",
-    version: "0.0.0.1", author: "Baka-Water77", note: null)]
-public class M9N
+namespace BakaWater77.M9N
 {
-    public bool isText { get; set; } = true;
-
-    [ScriptMethod(name: "魅亡之音", eventType: EventTypeEnum.StartCasting, eventCondition: new[] { "ActionId:regex:^([45921])$" })]
-    public void 魅亡之音(Event @event, ScriptAccessory accessory)
+    [ScriptType(name: "M9N", territorys: new[] { 1320 }, guid: "9af9ac60-1d6e-4247-a144-c6273417fea9",
+        version: "0.0.0.1", author: "Baka-Water77", note: null)]
+    public class M9N
     {
-        if (isText)
-            accessory.Method.TextInfo("AOE", duration: 4700, force: true);
-    }
+        // 脚本总开关
+        public bool ScriptEnabled { get; set; } = true;
 
-    [ScriptMethod(name: "以太流失", eventType: EventTypeEnum.StartCasting, eventCondition: new[] { "ActionId:regex:^(45896)$" })]
-    public void 以太流失(Event @event, ScriptAccessory accessory)
-    {
-        if (!@event["TargetId"].ParseObjectId(out var tid)) return;
+        // 单独文本开关
+        public bool isText { get; set; } = true;
 
-        var playerObj = accessory.Data.Objects.FirstOrDefault(x => x.GameObjectId == tid);
-        if (playerObj == null) return;
-
-        float rotation = playerObj.Rotation;
-
-        // 前后矩形
-        var dp1 = accessory.Data.GetDefaultDrawProperties();
-        dp1.Name = "以太流失";
-        dp1.Scale = new Vector2(6, 40);
-        dp1.ScaleMode = ScaleMode.ByTime;
-        dp1.Owner = tid;
-        dp1.Color = accessory.Data.DefaultDangerColor;
-        dp1.DestoryAt = 5000;
-        dp1.Rotation = rotation;
-        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp1);
-
-        // 左右矩形
-        var dp2 = accessory.Data.GetDefaultDrawProperties();
-        dp2.Name = "以太流失";
-        dp2.Scale = new Vector2(6, 40);
-        dp2.Rotation = rotation + MathF.PI / 2;
-        dp2.Color = accessory.Data.DefaultDangerColor;
-        dp2.DestoryAt = 5000;
-        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp2);
-    }
-
-    [ScriptMethod(name: "施虐的尖啸", eventType: EventTypeEnum.StartCasting, eventCondition: new[] { "ActionId:regex:^([45875])$" })]
-    public void 施虐的尖啸(Event @event, ScriptAccessory accessory)
-    {
-        if (isText)
-            accessory.Method.TextInfo("AOE", duration: 4700, force: true);
-    }
-
-    [ScriptMethod(name: "共振波", eventType: EventTypeEnum.StartCasting, eventCondition: new[] { "ActionId:regex:^([45901])$" })]
-    public void 共振波(Event @event, ScriptAccessory accessory)
-    {
-        var dp = accessory.Data.GetDefaultDrawProperties();
-        dp.Name = "共振波";
-        dp.Color = new Vector4(1.0f, 1.0f, 0.0f, 0.5f);
-        dp.ScaleMode = ScaleMode.ByTime;
-        dp.Position = @event.SourcePosition();
-        dp.Scale = new Vector2(8);
-        dp.DestoryAt = 7700;
-        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Circle, dp);
-    }
-}
-
-public static class M9NExtensions
-{
-    public static bool ParseObjectId(this string? idStr, out uint id)
-    {
-        id = 0;
-        if (string.IsNullOrEmpty(idStr)) return false;
-        try
+        [ScriptMethod(name: "魅亡之音", eventType: EventTypeEnum.StartCasting, eventCondition: new[] { "ActionId:regex:^([45921])$" })]
+        public void 魅亡之音(Event @event, ScriptAccessory accessory)
         {
-            var idStr2 = idStr.Replace("0x", "");
-            id = uint.Parse(idStr2, System.Globalization.NumberStyles.HexNumber);
-            return true;
+            if (!ScriptEnabled) return; // 脚本关闭时直接返回
+            if (isText)
+                accessory.Method.TextInfo("AOE", duration: 4700, force: true);
         }
-        catch
+
+        [ScriptMethod(name: "以太流失", eventType: EventTypeEnum.StartCasting, eventCondition: new[] { "ActionId:regex:^(45896)$" })]
+        public void 以太流失(Event @event, ScriptAccessory accessory)
         {
-            return false;
+            if (!ScriptEnabled) return;
+
+            if (!@event["TargetId"].ParseObjectId(out var tid)) return;
+
+            var playerObj = accessory.Data.Objects.FirstOrDefault(x => x.GameObjectId == tid);
+            if (playerObj == null) return;
+
+            float rotation = playerObj.Rotation;
+
+            // 前后矩形
+            var dp1 = accessory.Data.GetDefaultDrawProperties();
+            dp1.Name = "以太流失";
+            dp1.Scale = new Vector2(6, 40);
+            dp1.ScaleMode = ScaleMode.ByTime;
+            dp1.Owner = tid;
+            dp1.Color = accessory.Data.DefaultDangerColor;
+            dp1.DestoryAt = 5000;
+            dp1.Rotation = rotation;
+            accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp1);
+
+            // 左右矩形
+            var dp2 = accessory.Data.GetDefaultDrawProperties();
+            dp2.Name = "以太流失";
+            dp2.Scale = new Vector2(6, 40);
+            dp2.Rotation = rotation + MathF.PI / 2;
+            dp2.Color = accessory.Data.DefaultDangerColor;
+            dp2.DestoryAt = 5000;
+            accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp2);
+        }
+
+        [ScriptMethod(name: "施虐的尖啸", eventType: EventTypeEnum.StartCasting, eventCondition: new[] { "ActionId:regex:^([45875])$" })]
+        public void 施虐的尖啸(Event @event, ScriptAccessory accessory)
+        {
+            if (!ScriptEnabled) return;
+            if (isText)
+                accessory.Method.TextInfo("AOE", duration: 4700, force: true);
+        }
+
+        [ScriptMethod(name: "共振波", eventType: EventTypeEnum.StartCasting, eventCondition: new[] { "ActionId:regex:^([45901])$" })]
+        public void 共振波(Event @event, ScriptAccessory accessory)
+        {
+            if (!ScriptEnabled) return;
+
+            var dp = accessory.Data.GetDefaultDrawProperties();
+            dp.Name = "共振波";
+            dp.Color = new Vector4(1.0f, 1.0f, 0.0f, 0.5f);
+            dp.ScaleMode = ScaleMode.ByTime;
+            dp.Position = @event.SourcePosition();
+            dp.Scale = new Vector2(8);
+            dp.DestoryAt = 7700;
+            accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Circle, dp);
         }
     }
 
-    public static Vector3 SourcePosition(this Event @event)
+    public static class M9NExtensions
     {
-        if (@event == null || string.IsNullOrEmpty(@event["SourcePosition"]))
-            return Vector3.Zero;
+        public static bool ParseObjectId(this string? idStr, out uint id)
+        {
+            id = 0;
+            if (string.IsNullOrEmpty(idStr)) return false;
+            try
+            {
+                var idStr2 = idStr.Replace("0x", "");
+                id = uint.Parse(idStr2, System.Globalization.NumberStyles.HexNumber);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
-        return JsonConvert.DeserializeObject<Vector3>(@event["SourcePosition"]) ?? Vector3.Zero;
+        public static Vector3 SourcePosition(this Event @event)
+        {
+            if (@event == null || string.IsNullOrEmpty(@event["SourcePosition"]))
+                return Vector3.Zero;
+
+            return JsonConvert.DeserializeObject<Vector3>(@event["SourcePosition"]) ?? Vector3.Zero;
+        }
     }
 }
-
