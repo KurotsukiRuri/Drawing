38018756f61c0f2aef013d67c2f179445a69835a
黒月瑠璃 <86876700+KurotsukiRuri@users.noreply.github.com>
2025-12-20 00:50:35 +0800

    Update HW.cs

--- HW.cs
+++ HW.cs
@@ -12,12 +12,13 @@ namespace HelloWorld;
 [ScriptType(name: "HelloWorld", territorys: [], guid: "343daaf1-d3f8-4aa5-8c7f-0e08571dab33",
     version: "0.0.0.1", author: "Baka-Water77", note: null)]
 
-public class HelloWorld
+public class SampleMethod
 {
-    [ScriptMethod(name: "HelloWorld", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:24286"])]
-    public void HelloWorld(Event @event, ScriptAccessory accessory)
-    {
-        accessory.Method.TextInfo("Hello World!", 5000);
+   [ScriptMethod(name: "SampleMethod", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:24286"])]
+public void SampleMethod(Event @event, ScriptAccessory accessory)
+{   
+	accessory.Method.TextInfo("Hello World!", 5000);
+}
     }
 
 }
