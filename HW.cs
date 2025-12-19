e12036cf38581225e1885c08649bc8a24869b46d
黒月瑠璃 <86876700+KurotsukiRuri@users.noreply.github.com>
2025-12-20 00:54:10 +0800

    Update HW.cs

--- HW.cs
+++ HW.cs
@@ -1,24 +1,27 @@
 using System;
-using System.Linq;
 using System.Numerics;
-using Newtonsoft.Json;
-using Dalamud.Utility.Numerics;
 using KodakkuAssist.Script;
 using KodakkuAssist.Module.GameEvent;
-using KodakkuAssist.Module.Draw;
 
 namespace HelloWorld;
 
-[ScriptType(name: "HelloWorld", territorys: [], guid: "343daaf1-d3f8-4aa5-8c7f-0e08571dab33",
-    version: "0.0.0.1", author: "Baka-Water77", note: null)]
-
-public class SampleMethod
+[ScriptType(
+    name: "HelloWorld",
+    territorys: new uint[] { },
+    guid: "AF4E8C43-08BA-4C98-8DEF-70F023AF234D",
+    version: "0.0.0.1",
+    author: "Baka-Water77",
+    note: null
+)]
+public class HelloWorld
 {
-   [ScriptMethod(name: "SampleMethod", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:24286"])]
-public void SampleMethod(Event @event, ScriptAccessory accessory)
-{   
-	accessory.Method.TextInfo("Hello World!", 5000);
-}
+    [ScriptMethod(
+        name: "HelloWorld",
+        eventType: EventTypeEnum.StartCasting,
+        eventCondition: ["ActionId:24286"]
+    )]
+    public void HelloWorldCast(Event @event, ScriptAccessory accessory)
+    {
+        accessory.Method.TextInfo("Hello World!", 5000);
     }
-
 }
