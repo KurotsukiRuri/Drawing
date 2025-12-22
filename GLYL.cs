862bbcac73518b356127b0a112e09e2faa483cda
水水 <86876700+KurotsukiRuri@users.noreply.github.com>
2025-12-22 16:20:49 +0800

    Update GLYL.cs

--- GLYL.cs
+++ GLYL.cs
@@ -27,7 +27,7 @@
        name: "极格莱杨拉",
        territorys: new uint[] { 1308 },
        guid: "125b0e7e-1fcc-412f-9d70-49d0ba2a6e3f",
-       version: "0.0.0.4",
+       version: "0.0.0.2",
        author: "Baka-Water77",
        note: null
     )]
@@ -76,7 +76,6 @@
                     lastTargetIdForScatter = TargetId;
 
                
-            ClearLastTargetIds();
             return;
             }
 
