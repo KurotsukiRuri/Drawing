738b247832ea6297d7cfae99301096ec43984ee6
水水 <86876700+KurotsukiRuri@users.noreply.github.com>
2025-12-22 11:03:58 +0800

    Update GLYL.cs

--- GLYL.cs
+++ GLYL.cs
@@ -24,18 +24,143 @@ namespace BakaWater77.极格莱杨拉;
 
 
 [ScriptType(
-   name: "极格莱杨拉P2异世界",
+   name: "极格莱杨拉",
    territorys: new uint[] { 1308 },
    guid: "125b0e7e-1fcc-412f-9d70-49d0ba2a6e3f",
-   version: "0.0.0.1",
+   version: "0.0.0.2",
    author: "Baka-Water77",
    note: null
 )]
 public class 极格莱杨拉
 {
     public bool isText { get; set; } = true;
+     private static bool ParseObjectId(string? idStr, out uint id)
+    {
+        id = 0;
+        if (string.IsNullOrEmpty(idStr)) return false;
 
-    
+        try
+        {
+            id = uint.Parse(idStr.Replace("0x", ""), System.Globalization.NumberStyles.HexNumber);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    [ScriptMethod(
+    name: "超增压",//分散
+    eventType: EventTypeEnum.StartCasting,
+    eventCondition: new[] { "ActionId:regex:^(45663)$" },
+    userControl: true
+)]
+    public void 超增压分散(Event @event, ScriptAccessory accessory)
+    {
+        if (isText)
+            accessory.Method.TextInfo("分散", duration: 4700);
+        var ALLmember = new[]
+        {
+        (Index: 0, Name: "MT"),
+        (Index: 1, Name: "ST"),
+        (Index: 2, Name: "H1"),
+        (Index: 3, Name: "H2"),
+        (Index: 4, Name: "D1"),
+        (Index: 5, Name: "D2"),
+        (Index: 6, Name: "D3"),
+        (Index: 7, Name: "D4")
+    };
+
+
+        foreach (var (index, name) in ALLmember)
+        {
+            var memberObj = accessory.Data.PartyList[index];
+            var dp = accessory.Data.GetDefaultDrawProperties();
+            dp.Name = "超增压分散";
+            dp.Owner = memberObj;
+            dp.Scale = new Vector2(5);
+            dp.Color = accessory.Data.DefaultDangerColor;
+            dp.DestoryAt = 6000;
+            accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Circle, dp);
+        }
+    }
+    [ScriptMethod(
+    name: "超增压",//分摊
+    eventType: EventTypeEnum.StartCasting,
+    eventCondition: new[] { "ActionId:regex:^(45664)$" },
+    userControl: true
+)]
+    public void 超增压分摊4TN(Event @event, ScriptAccessory accessory)
+    {
+        if (isText)
+            accessory.Method.TextInfo("分摊", duration: 4700);
+        if (!ParseObjectId(@event["TargetId"], out uint TargetId))
+            return;
+        if (TargetId == 0x40002AF7)
+        {
+            var fourTN = new[]
+        {
+        (Index: 0, Name: "MT"),
+        (Index: 1, Name: "ST"),
+        (Index: 2, Name: "H1"),
+        (Index: 3, Name: "H2")
+
+    };
+            foreach (var (index, name) in fourTN)
+            {
+                var memberObj = accessory.Data.PartyList[index];
+                var dp = accessory.Data.GetDefaultDrawProperties();
+                dp.Name = "超增压分摊";
+                dp.Owner = memberObj;
+                dp.Scale = new Vector2(5);
+                dp.Color = accessory.Data.DefaultSafeColor;
+                dp.DestoryAt = 6000;
+                accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Circle, dp);
+            }
+        }
+
+
+    }
+
+    [ScriptMethod(
+    name: "超增压",//分摊
+    eventType: EventTypeEnum.StartCasting,
+    eventCondition: new[] { "ActionId:regex:^(45664)$" },
+    userControl: true
+)]
+    public void 超增压分摊4DPS(Event @event, ScriptAccessory accessory)
+    {
+        if (isText)
+            accessory.Method.TextInfo("分摊", duration: 4700);
+        if (!ParseObjectId(@event["TargetId"], out uint TargetId))
+            return;
+        if (TargetId == 0x40002AF7)
+        {
+            var fourDPS = new[]
+        {
+        (Index: 4, Name: "D1"),
+        (Index: 5, Name: "D2"),
+        (Index: 6, Name: "D3"),
+        (Index: 7, Name: "D4")
+
+    };
+            foreach (var (index, name) in fourDPS)
+            {
+                var memberObj = accessory.Data.PartyList[index];
+                var dp = accessory.Data.GetDefaultDrawProperties();
+                dp.Name = "超增压分摊";
+                dp.Owner = memberObj;
+                dp.Scale = new Vector2(5);
+                dp.Color = accessory.Data.DefaultSafeColor;
+                dp.DestoryAt = 6000;
+                accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Circle, dp);
+            }
+        }
+
+
+    }
+
     [ScriptMethod(
     name: "以太炮",
     eventType: EventTypeEnum.TargetIcon,
